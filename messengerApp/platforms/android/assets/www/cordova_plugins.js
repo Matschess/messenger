7028b68abf514f4cd38d0a24035b9bd70419fa8b
Matthias Lang <langmattis@gmail.com>
2016-06-17 13:54:27 +0200

    Adds Messenger App
    - adds messenger app directory

---
+++ messengerApp/platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,61 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-vibration/www/vibration.js",
+        "id": "cordova-plugin-vibration.notification",
+        "merges": [
+            "navigator.notification",
+            "navigator"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-background-mode/www/background-mode.js",
+        "id": "cordova-plugin-background-mode.BackgroundMode",
+        "clobbers": [
+            "cordova.plugins.backgroundMode",
+            "plugin.backgroundMode"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+        "id": "cordova-plugin-statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-autostart/www/auto-start.js",
+        "id": "cordova-plugin-autostart.AutoStart",
+        "clobbers": [
+            "cordova.plugins.autoStart"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.2.2",
+    "cordova-plugin-inappbrowser": "1.4.0",
+    "cordova-plugin-vibration": "2.1.1",
+    "cordova-plugin-device": "1.1.2",
+    "cordova-plugin-background-mode": "0.6.5",
+    "cordova-plugin-statusbar": "2.1.3",
+    "cordova-plugin-autostart": "2.0.1"
+};
+// BOTTOM OF METADATA
+});
